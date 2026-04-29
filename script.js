89089c3863063ab936966d63df2bbf148516f967
common-star00 <commonkay00@gmail.com>
2026-04-29 18:29:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+document.querySelectorAll(".menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    const menu = document.getElementById("menu");
+    menu.style.display = "none";
+  });
+});
